b4c1515a46fd7ae8fe940703c0116c5804072be4
nguyendinson323 <nguyendinson323@gmail.com>
2025-08-12 16:10:03 -0500

    changed port

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const apiRoutes = require('./routes');
 
 // Initialize Express app
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const API_VERSION = process.env.API_VERSION || 'v1';
 
 // Security middleware
